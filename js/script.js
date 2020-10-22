e5c76043b554d4dd747e206164bf787a97126f2b
alessandro <alessandro.massarella@gmail.com>
2020-10-22 17:27:14 +0200

    eMail - FIX

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ var friendMail = ['pippo@gmail.com', 'pluto@gmail.com', 'topolino@gmail.com', 'm
 
 // chiedo all'utente di inserire la propria Mail
 var mailUtente = prompt ('Inserisci la tua mail');
+var messaggio = ('mail NON trovata');
 
-
+document.getElementById('mail').innerHTML = (messaggio);
 
 // eseguo il ciclo che compara la mail inserita con quelle dell'Array()
 for (var i = 0; i < friendMail.length; i++) {
   if (friendMail[i] == mailUtente) {
     document.getElementById('mail').innerHTML = ('mail in archivio');
-  } else if {
-    (friendMail[i] !== mailUtente)
-  } 
-    document.getElementById('mail').innerHTML = ('mail NON in archivio');
+  }
 }
+
+
+
+
+// // GIOCO DEI DADI
+// var myButtonEl = document.getElementById ('myButton');
+// myButtonEl.addEventListener('click', function()) {
+//
+// }
